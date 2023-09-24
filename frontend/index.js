68fa4fc2db506798de32d5b929738027828d7da3
ron34353435 <ron34353435@gmail.com>
2023-09-24 13:43:40 +0300

    first web page displayed

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,12 @@
+const path = require('path');
 const express = require('express');
+
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'www')))
+
 app.get('/', (req, res) => {
-    res.send({ message: 'Hello World!' });
+    res.sendFile('profile/index.html');
 });
 
 app.listen(3000, () => {
